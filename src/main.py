78a8677ef45621ed70a59d61b68d2a4dc906bb3b
kangfengshou <kangfengshou@turingq.com>
2024-09-06 15:26:00 +0800

    modify main class signature, the config file and lateration for arbitrary laterate edge num

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ def parse_arguments():
     parser.add_argument("--beacon_coords_file", type=str, default="../datas/data0827a/beacon_coords.txt", help="Path to the file containing beacon coordinates")
     parser.add_argument("--beacon_data_file", type=str, default="../datas/data0827a/bluetoothData(58).txt", help="Path to the file containing beacon data")
     parser.add_argument("--update_frequency", type=float, default=1.0, help="Update frequency in Hz")
+    parser.add_argument("--config_file", type=str, default="../config/beacon_config.yaml", help="config file")
     return parser.parse_args()
 
 
